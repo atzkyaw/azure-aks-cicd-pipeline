794542e677893ccbce21dc654ce0898e7334d13c
Aye Thazin Kyaw <atzkyaw@gmail.com>
2025-06-25 17:41:35 +0800

    updated application code

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -1,14 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyApp.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+namespace WebApplication2.Controllers
 {
-    [HttpGet]
-    public IEnumerable<string> Get()
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
     {
-        return new[] { "Sunny", "Cloudy", "Rainy" };
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
